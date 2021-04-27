cac602ca8934b13c53b798a2d6260ea679ef6a3b
Ryan Caezar Itang <sitiom@disroot.org>
2021-04-27 14:27:21 +0800

    Add animation on element show

--- Unplastify/wwwroot/js/site.js
+++ Unplastify/wwwroot/js/site.js
@@ -2,3 +2,25 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+// Scroll function courtesy of Scott Dowding; http://stackoverflow.com/questions/487073/check-if-element-is-visible-after-scrolling (Made to vanilla)
+document.addEventListener('DOMContentLoaded', function () {
+    // Check if element is scrolled into view
+    function isScrolledIntoView(el) {
+        var rect = el.getBoundingClientRect();
+        var elemTop = rect.top;
+        var elemBottom = rect.bottom;
+
+        // Partially visible elements return true:
+        var isVisible = elemTop < window.innerHeight && elemBottom >= 0;
+        return isVisible;
+    }
+    // If element is scrolled into view, fade it in
+    window.addEventListener('scroll', function () {
+        document.querySelectorAll('.animate__animated').forEach(function (element, index) {
+            if (isScrolledIntoView(element) === true) {
+                element.classList.add('animate__fadeInUp');
+            }
+        });
+    });
+})
